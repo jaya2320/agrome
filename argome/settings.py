c1a2ef2dcabe0a56fa4dd856b04cc5bc7fbb2433
jaya2320 <jayagupta752@gmail.com>
2021-03-05 13:50:49 +0530

    first commit

--- argome/settings.py
+++ argome/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
